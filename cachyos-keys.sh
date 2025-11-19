28eb39894fd8ae7fafb4bf1add72b456e1b54078
Lars Slettan Øydneskleiv <okstudios96@gmail.com>
2025-11-19 04:17:06 +0100

    Update and rename cachyos.sh to cachyos-keys.sh

---
+++ cachyos-keys.sh
@@ -0,0 +1,2 @@
+pacman-key --recv-keys F3B607488DB35A47 --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key F3B607488DB35A47
--- cachyos.sh
+++
@@ -1,4 +0,0 @@
-pacman-key --recv-keys F3B607488DB35A47 --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key F3B607488DB35A47
-pacman -U --noconfirm \
-  https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-\{keyring-20240331-1,mirrorlist-22-1,v4-mirrorlist-22-1\}-any.pkg.tar.zst
