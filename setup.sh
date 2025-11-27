e24312c2eaa4cb118fcb7dfe1fb8c9700ba9422b
Lars Slettan Øydneskleiv <okstudios96@gmail.com>
2025-11-27 16:40:33 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,8 +5,10 @@ pacman-key --recv-keys F3B607488DB35A47 --keyserver keyserver.ubuntu.com
 # Sign the repository key
 pacman-key --lsign-key F3B607488DB35A47
 
-# Install the necessary mirrors:
+# Install the necessary packages:
 pacman -U 'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-keyring-20240331-1-any.pkg.tar.zst' \
 'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-mirrorlist-22-1-any.pkg.tar.zst' \
 'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-v3-mirrorlist-22-1-any.pkg.tar.zst' \
-'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-v4-mirrorlist-22-1-any.pkg.tar.zst'
+'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-v4-mirrorlist-22-1-any.pkg.tar.zst' \
+'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-rate-mirrors-17-1-any.pkg.tar.zst'
+
